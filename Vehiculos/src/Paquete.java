a2ecf346b4fca9657220c03c9036b7e1110243f8
ldcvelez <ldc.mail2@gmail.com>
2020-09-28 22:28:39 -0300

    Clase Vehiculo con Herencia

---
+++ Vehiculos/src/Paquete.java
@@ -0,0 +1,26 @@
+
+public class Paquete {
+
+	private String destino;
+	private int peso;
+	
+	public Paquete(String destino, int peso) {
+		this.destino = destino;
+		this.peso = peso;
+	}
+
+	public String getDestino() {
+		return destino;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	@Override
+	public String toString() {
+		return "Paquete [destino=" + destino + ", peso=" + peso + "]";
+	}
+	
+	
+}
---
+++ Vehiculos/src/Vehiculo.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public abstract class Vehiculo {
+	
+	protected int carga = 0;
+	private List<Paquete> paquetes = new LinkedList<Paquete>();
+	public abstract boolean puedeLlevar(Paquete paquete);
+	
+	public final void cargar(Paquete paquete) {
+		if (puedeLlevar(paquete)) {
+			this.paquetes.add(paquete);
+			this.carga += paquete.getPeso();
+		}
+	}
+
+	@Override
+	public String toString() {
+		return this.getClass().getName() + " [carga=" + carga + ", paquetes=" + paquetes + "]";
+	}
+}
